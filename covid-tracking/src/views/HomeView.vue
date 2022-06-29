cdecf6b096681d0b6d7a59e3f593e8371a6bb035
lycho33 <cho.lyds@gmail.com>
2022-06-29 18:42:42 -0400

    fetch request with conditionals

--- covid-tracking/src/views/HomeView.vue
+++ covid-tracking/src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="home">
-    Hello World
-  </div>
+  <main  v-if="!loading" class="home">
+    Show Data
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <!-- mt: margin-top -->
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <!-- w: width, m: margin -->
+    <img :src="loadingImage" class="w-24 m-auto" alt="">
+  </main>
 </template>
 
 
@@ -12,13 +21,30 @@
 export default {
   name: 'HomeView',
   components: {},
-  methods: {
-    async fetchCovidData(){
-      const res = await fetch('')
+  data(){
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      status: {},
+      countries: [],
+      loadingImage: require('../assets/blue-hourglass.gif'),
     }
   },
-  created(){
+  methods: {
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created(){
     console.log("created")
+    const data = await this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = false
   }
 }
 </script>
