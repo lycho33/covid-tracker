e534bb7dac09838ae4d95b4a8f7e9f54f37a9644
lycho33 <cho.lyds@gmail.com>
2022-06-29 18:03:17 -0400

    add Header component

--- covid-tracking/src/App.vue
+++ covid-tracking/src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  <router-view/>
+  <Header />
+  <div class="container">
+    <router-view/>
+  </div>
+
 </template>
+
+<script>
+  import Header from '@/components/Header'
+
+  export default {
+    components: {
+      Header
+    }
+  }
+</script>
+
