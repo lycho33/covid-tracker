9b97f61f21639937244973a054549bb6b5a065b6
lycho33 <cho.lyds@gmail.com>
2022-07-13 23:51:47 -0400

    styling

--- covid-tracking/src/components/Header.vue
+++ covid-tracking/src/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>working?</h1>
+  <header class="text-center bg-blue-800 p-4 mb-10 text-white">
+    <div 
+      class="text-3xl md:text-5xl font-bold mb-3"
+    >
+      COVID TRACKER
+    </div>
+  </header>
 </template>
 
 <script>
