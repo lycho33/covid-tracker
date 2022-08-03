f2e4cd420ed3982d0af25518052fc3b7d39ed275
lycho33 <cho.lyds@gmail.com>
2022-08-02 22:25:49 -0400

    styling + render more stats

--- covid-tracking/src/components/DataBoxes.vue
+++ covid-tracking/src/components/DataBoxes.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="grid md:grid-cols-2 gap-4">
-        <div class="shadow-md bg-blue-100 p-100 text-center rounded">
+        <!-- Box 1 -->
+        <div class="shadow-md bg-blue-100 p-10 text-center rounded">
             <h3 class="text-3xl text-blue-900 font-bold mb-4">
                 Cases
             </h3>
@@ -12,6 +13,21 @@
             <div class="text-2xl mb-4">
                 <span class="font-bold">Total:</span>
                 {{stats.TotalConfirmed}}
+            </div>
+        </div>
+        <!-- Box 2  -->
+        <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">
+                Deaths
+            </h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{stats.NewDeaths}}             
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{stats.TotalDeaths}}
             </div>
         </div>
     </div>
